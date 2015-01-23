e76d0c16865103ab53b900c7009d092b488d186c
Joshua Ramsbottom <joshuaramsbottom@gmail.com>
2015-01-24 00:48:17 +0200

    Recycling floor

--- Assets/Scripts/FloorManager.cs
+++ Assets/Scripts/FloorManager.cs
@@ -3,13 +3,32 @@ using System.Collections;
 
 public class FloorManager : MonoBehaviour {
 
+	//Array of floor pieces
+	public GameObject[] floorArr = new GameObject[3];
+
+	private Vector3 floorSize;
+
 	// Use this for initialization
 	void Start () {
-	
+		floorSize = floorArr[0].transform.renderer.bounds.max - floorArr[0].transform.renderer.bounds.min;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (floorArr[0].transform.position.x < Camera.main.transform.position.x) {
+			if (!floorArr[0].transform.renderer.IsVisibleFrom(Camera.main)) {
+				//get position of last floor piece
+				Transform lastFloor = floorArr[floorArr.Length-1].transform;
+
+				floorArr[0].transform.position = new Vector3(lastFloor.position.x + floorSize.x, floorArr[0].transform.position.y, 0);
+
+				//Shuffle all the pieces down
+				GameObject temp = floorArr[0];
+				for (int i=0; i < floorArr.Length - 1; i++) {
+					floorArr[i] = floorArr[i + 1];
+				}
+				floorArr[floorArr.Length - 1] = temp;
+			}
+		}
 	}
 }
