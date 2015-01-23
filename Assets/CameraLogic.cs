c7706ffbf8fecafe6f257c8c1e397404d0c618df
creative630 <creative630@gmail.com>
2015-01-23 23:40:24 +0200

    Initial runner physics

---
+++ Assets/CameraLogic.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraLogic : MonoBehaviour 
+{
+    public Vector3 moveVel = new Vector3(0.0f, 0.0f);
+    public static CameraLogic camLogic;
+
+    void Awake()
+    {
+        camLogic = this;
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        Vector3 currentPos =  gameObject.transform.position;
+        currentPos += moveVel * Time.deltaTime;
+        currentPos.z = -10;
+
+        gameObject.transform.position = currentPos;
+
+
+
+	}
+}
