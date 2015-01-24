f5ad7b7c7bdb8725c16c3bc63717b477283702bf
creative630 <creative630@gmail.com>
2015-01-24 12:56:13 +0200

    Fixed jumping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,9 @@ public class PlayerMovement : MonoBehaviour
     public Vector3 rightAcceleration = Vector3.zero;
     public Vector3 maxVel = Vector3.zero;
     public float jumpVel = 0.0f;
+
+    public float raycastLength = 3.0f;
+    public LayerMask mask;
 	// Use this for initialization
 	void Start () 
     {
@@ -19,18 +22,31 @@ public class PlayerMovement : MonoBehaviour
 	// Update is called once per frame
 	void FixedUpdate () 
     {
-        //Vector3 pos = gameObject.rigidbody2D.position;
-        //pos.x += moveVel.x;
-        //gameObject.transform.position = pos;
-
+        
         Vector3 vel = gameObject.rigidbody2D.velocity;
         vel += rightAcceleration*Time.deltaTime;
 
+        Vector2 position = (Vector2)transform.position;
+        Vector2 down = -Vector2.up * raycastLength;
+        Debug.DrawRay(transform.position, down, Color.green);
+        //bool onTheGround = Physics.Raycast(transform.position, Vector3.down, raycastLength);
+        RaycastHit2D hit;
+        //Ray2D ray = new Ray2D(transform.position, down);
+        //rigidbody2D.
 
-        if (Input.GetKey(KeyCode.UpArrow))
+        bool onTheGround = Physics2D.Raycast(position, down, raycastLength,~mask.value);
+
+
+        if (onTheGround)
         {
-            //Vector3 vel = gameObject.rigidbody2D.velocity;
-            vel.y += jumpVel* Time.deltaTime;
+            print("There is something below the object!");
+        }
+        if (Input.GetKey(KeyCode.UpArrow) && onTheGround)
+        {
+
+
+            vel.y += jumpVel * Time.deltaTime;
+
         }
 
         if (vel.x >= maxVel.x)
