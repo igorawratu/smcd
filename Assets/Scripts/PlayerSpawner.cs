18cfe53d1323a6b9720019935f95dc77ee77f477
Joshua Ramsbottom <joshuaramsbottom@gmail.com>
2015-01-24 22:16:46 +0200

    Added players spawning at the bottom

--- Assets/Scripts/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner.cs
@@ -8,15 +8,23 @@ public class PlayerSpawner : MonoBehaviour {
     public float startPoint = 0.0f;
 	// Use this for initialization
 	void Start () {
-		foreach (KeyCode kc in CurrentPlayerKeys.Instance.playerKeys) {
+		for (int i = 0; i < CurrentPlayerKeys.Instance.playerKeys.Count; i++) {
+			KeyCode kc = CurrentPlayerKeys.Instance.playerKeys[i];
 			GameObject player = (GameObject) Instantiate(playerPrefab);
 			player.name = kc.ToString();
 			player.GetComponent<PlayerMovement>().setJumpKey(kc);
 
 			//Random player's spawn position near the middle
+<<<<<<< HEAD
 			offset = Random.Range(0, 2);
             player.transform.position = new Vector3(startPoint + offset, 0.59f, 0);
+=======
+			float offset = Random.Range(0, 2);
+			player.transform.position = new Vector3(-4 - offset, 1.65f, 0);
+>>>>>>> Added players spawning at the bottom
 
+			//Assign player color
+			player.GetComponent<PlayerMovement>().playerColour = CurrentPlayerKeys.Instance.playerColors[i];
 		}
 	}
 	
