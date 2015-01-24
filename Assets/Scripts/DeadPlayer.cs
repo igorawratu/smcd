086b5accdae807e8fa9cd0ec649beb5420ff3d35
Sunrise Wang <sunrisewng@gmail.com>
2015-01-24 16:03:06 +0200

    dead player logic and spawning

---
+++ Assets/Scripts/DeadPlayer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeadPlayer : MonoBehaviour {
+    private string mPKey;
+
+	// Use this for initialization
+    void Awake()
+    {
+        mPKey = "a";
+        SpriteRenderer sr = gameObject.GetComponent<SpriteRenderer>();
+        sr.color = new Color(255, 0, 0);
+        Debug.Log("Start called");
+    }
+
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate(){
+        if (gameObject.transform.position.y < 0)
+        {
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x, 0);
+            gameObject.rigidbody2D.velocity = new Vector2(gameObject.rigidbody2D.velocity.x, 0);
+        }
+
+        if (Input.GetKey(mPKey)){
+            gameObject.rigidbody2D.velocity = new Vector2(gameObject.rigidbody2D.velocity.x, 5);
+        }
+    }
+
+    public void setInfo(string _key, Color _col){
+        mPKey = _key;
+        gameObject.name = "dead" + mPKey;
+        SpriteRenderer sr = gameObject.GetComponent<SpriteRenderer>();
+        sr.color = _col;
+        Debug.Log("Setinfo called");
+    }
+}
