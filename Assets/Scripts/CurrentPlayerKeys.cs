5de2e356a32ca4f8c07581f9fe4e8be5999f471f
Joshua Ramsbottom <joshuaramsbottom@gmail.com>
2015-01-24 16:42:49 +0200

    Added first Menu Scene

---
+++ Assets/Scripts/CurrentPlayerKeys.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CurrentPlayerKeys : Singleton<CurrentPlayerKeys> {
+	protected CurrentPlayerKeys () {} // guarantee this will be always a singleton only - can't use the constructor!
+	
+	public List<KeyCode> playerKeys = new List<KeyCode>();
+}
